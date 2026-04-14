2a5977765708965df7cb7b1f11279a06e4a813ee
Tahar Meijs <tntmeijs@gmail.com>
2026-04-14 19:57:43 +0200

    feat: use alpine lts image

--- scripts/download_alpine_runtime.sh
+++ scripts/download_alpine_runtime.sh
@@ -18,32 +18,34 @@ release_url="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/${ARCH
 # Grab the manifest and figure out which release is the latest release.
 wget -nc "${release_url}/latest-releases.yaml"
 rootFsName=$(yq '.. | select(has("flavor")) | select(.flavor == "alpine-minirootfs") | .file' ./latest-releases.yaml)
-osName=$(yq '.. | select(has("flavor")) | select(.flavor == "alpine-netboot") | .file' ./latest-releases.yaml)
+osName=$(yq '.. | select(has("flavor")) | select(.flavor == "alpine-standard") | .file' ./latest-releases.yaml)
+
+# Download Alpine Linux distribution ISO and grab the compressed Linux kernel.
+wget -O alpine.iso "${release_url}/${osName}"
+mkdir ./alpine
+sudo mount -o loop alpine.iso ./alpine
+cp ./alpine/boot/vmlinuz-lts ./vmlinuz
+sudo umount ./alpine
+
+# Alpine Linux is distributed with a compressed Linux kernel.
+# To make this work with Firecracker, we need to uncompress it into vmlinux-virt.
+wget -nc https://raw.githubusercontent.com/torvalds/linux/refs/heads/master/scripts/extract-vmlinux
+chmod +x ./extract-vmlinux
+./extract-vmlinux "./vmlinuz" > "./vmlinux"
 
 # Download and configure rootFs.
-wget "${release_url}/${rootFsName}"
+wget -nc "${release_url}/${rootFsName}"
 tar -xzf ${rootFsName} --one-top-level
 rootFsName=$(ls -d */ | grep "alpine-minirootfs")
 rootFsName=${rootFsName%/}
-# TODO: configure rootFs here
+cp "${repo_root}/scripts/init" "${rootFsName}"/init
+
 sudo chown -R root:root ${rootFsName}
-truncate -s 16MB "${rootFsName}.ext4"
+truncate -s 128MB "${rootFsName}.ext4"
 sudo mkfs.ext4 -d ${rootFsName} -F "${rootFsName}.ext4"
-
-# Download Alpine Linux distribution.
-wget "${release_url}/${osName}"
-tar -xzf ${osName} --one-top-level
-osName=$(ls -d */ | grep "alpine-netboot")
-osName=${osName%/}
-
-# Alpine Linux is distributed with a compressed Linux kernel: vmlinuz-virt.
-# To make this work with Firecracker, we need to uncompress it into vmlinux-virt.
-wget https://raw.githubusercontent.com/torvalds/linux/refs/heads/master/scripts/extract-vmlinux
-chmod +x ./extract-vmlinux
-./extract-vmlinux "${osName}/boot/vmlinuz-virt" > vmlinux
 
 # All of this is needed by Firecracker.
 mv "./${rootFsName}.ext4" "${target_dir}/rootfs.ext4"
-mv ./vmlinux "${target_dir}/vmlinux"
+mv "./vmlinux" "${target_dir}/vmlinux"
 
 popd > /dev/null
